44da1a3009fa24309fcbcf32a8aed638ec6aede7
Alvinarioyt <88835109+Alvinarioyt@users.noreply.github.com>
2023-07-31 10:32:26 +0800

    Create dfs.js

---
+++ dfs.js
@@ -0,0 +1,3 @@
+<a rel="ar" id="ApplePay" href="alarm‑clock.usdz#applePayButtonType=plain&checkoutTitle=Retro%20Alarm%20Clock&checkoutSubtitle=With%20built-in%20FM%20tuner&price=$92.50">
+    <img src="alarm-clock-thumbnail.png">
+</a>
